4b1c0d5bbf6a8646fc7587b67021b50cc4fbc44f
Gregor Woiwode <gregor.woiwode@gmail.com>
2015-10-14 18:57:34 +0200

    Uses book model in RankedBook component

--- book-ranking-code/app/components/book-ranking.ts
+++ book-ranking-code/app/components/book-ranking.ts
@@ -1,14 +1,42 @@
-import {Component, View} from 'angular2/angular2'
+import {Component, View, NgFor} from 'angular2/angular2';
+import Book from '../models/book';
+import RankedBook from './ranked-book';
+
 @Component({
   selector: 'book-ranking'
 })
 @View({
-  template: `<h1>Books</h1><p>{{ books }}</p>`
+  directives: [RankedBook, NgFor],
+  template: `
+     <section class="new-link">
+       <div class="control-group">
+         <div><label for="title">Title:</label></div>
+         <div><input name="title" #title></div>
+       </div>
+       <div class="control-group">
+         <div><label for="link">Link:</label></div>
+         <div><input name="link" #link/></div>
+       </div>
+       <button (click)="add(title, link)">Submit</button>
+     </section>
+     <book *ng-for="#book of books" [book]="book"></book>
+   `
 })
 export default class BookRanking {
-  books: Array<string>;
+  books: Array<Book>;
 
   constructor() {
-    this.books = ['bookA','bookB'];
+    this.books = [
+      new Book('ExampleBook', 'http://mybook.org'),
+      new Book('AnotherExample', 'http://yourbook.com')
+    ];
+  }
+
+  add(title, link) {
+    var newBook = new Book(title.value, link.value);
+    this.books.push(newBook);
+
+    title.value = '';
+    link.value = '';
   }
 }
