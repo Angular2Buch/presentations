2323a9c95fe4b433da64a08730dcbe5c6b6ddee4
Gregor Woiwode <gregor.woiwode@gmail.com>
2015-10-15 00:23:49 +0200

    Adds slides for event communication

---
+++ book-rating/samples/components/book-rating/book-rating-subscribe-to-event.ts
@@ -0,0 +1,23 @@
+import {Component, View, NgFor} from 'angular2/angular2';
+import Book from '../models/book';
+import BookComponent from './book-component';
+
+@Component({
+  selector: 'book-rating'
+})
+@View({
+  directives: [BookComponent, NgFor],
+  template: `
+     <!-- ... -->
+     <book ... (rated)="reorderBooks(book)"></book>
+   `
+})
+export default class BookRating {
+  books: Array<Book>;
+
+  /* ... */
+
+  reorderBooks(book: Book) {
+    this.books.sort((a, b) => b.rating - a.rating);
+  }
+}
