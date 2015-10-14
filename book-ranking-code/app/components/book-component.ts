58b9a35033d645a13fc3a8b5696e2f74144f619f
Gregor Woiwode <gregor.woiwode@gmail.com>
2015-10-14 21:17:27 +0200

    Adds rating for a book

--- book-ranking-code/app/components/book-component.ts
+++ book-ranking-code/app/components/book-component.ts
@@ -2,17 +2,29 @@ import { Component, View } from 'angular2/angular2';
 import Book from '../models/book';
 
 @Component({
-  selector: 'book', properties: ['book']
+  selector: 'book',
+  properties: ['book'],
 })
 @View({
   template: `
     <div class="main">
       <h2>
-        <a href="{{ book.link }}">{{ book.title }}</a>
+        <a href="{{ book.link }}">{{ book.title }} {{ book.rating }}</a>
+        <button (click)="voteUp()">Vote up</button>
+        <button (click)="voteDown()">Vote down</button>
+        <hr />
       </h2>
     </div>
   `
 })
 export default class BookComponent {
   book: Book;
+
+  voteUp() {
+    this.book.rating++;
+  }
+
+  voteDown() {
+    this.book.rating--;
+  }
 }
--- book-ranking-code/app/models/book.ts
+++ book-ranking-code/app/models/book.ts
@@ -1,6 +1,7 @@
 export default class Book {
   title: string;
   link: string;
+  rating: number = 0;
 
   constructor(title, link) {
     this.title = title;
